de9f8dffefcecbb6cd33f56129eaa28025426aa8
Shubh Saraswat <shubhsaraswat.95@gmail.com>
2024-07-25 23:49:48 -0400

    added new functions

---
+++ R/cyt.bp2.R
@@ -0,0 +1,63 @@
+#' Boxplot Function Enhanced for Specific Group Comparisons
+#'
+#' @param x.df A matrix or data frame of raw data.
+#' @param Title The title of the PDF file.
+#' @param scale Transformation option for continuous variables. Options are NULL (default) and "log2".
+#' @param yLim A range for the y-axis to be plotted.
+#'
+#' @return A PDF file containing boxplots of the data.
+#' @export
+#'
+#' @examples
+#'data.df = cytdata.df[,-c(1,4)]
+#'cyt.bp2(data.df, Title = "boxplot2.test2.pdf", scale = "log2")
+
+cyt.bp2 <- function(x.df, Title, mfRow=c(1,1), scale = NULL, yLim=NULL) {
+
+  # Convert any character variables to factors
+  cat_vars <- sapply(x.df, is.character)
+  if(any(cat_vars)){
+    x.df[cat_vars] <- lapply(x.df[cat_vars], as.factor)
+  }
+
+  # Identify numeric and factor columns
+  num_cols <- sapply(x.df, is.numeric)
+  fac_cols <- sapply(x.df, is.factor)
+
+  # Ensure there is at least one numeric and one factor column
+  if(!any(num_cols)) stop("Data must contain at least one numeric column")
+  if(!any(fac_cols)) stop("Data must contain at least one factor column")
+
+  # Apply log2 transformation if log_scale is TRUE
+  if(!is.null(scale) && scale == "log2"){
+    x.df[num_cols] <- lapply(x.df[num_cols], function(x) log2(x))
+  }
+
+  # Generate boxplots
+  pdf(file=Title)
+  par(mfrow=mfRow, cex.axis=0.75)
+
+  # Loop through each numeric column
+  for(num_col in names(x.df)[num_cols]) {
+    # Loop through each factor column
+    for(fac_col in names(x.df)[fac_cols]) {
+      # Create the boxplot
+      p <- ggplot(x.df, aes_string(x = fac_col, y = num_col, fill = fac_col)) +
+        geom_boxplot(alpha = 0.5) +
+        geom_jitter(width = 0.2, alpha = 0.5) +
+        labs(title = paste0(num_col, " by ", fac_col),
+             x = fac_col, y = paste0("Values of ", num_col)) +
+        theme_minimal() +
+        theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5)) +
+        guides(fill = guide_legend(title = NULL))
+
+      if(!is.null(yLim)) {
+        p <- p + ylim(yLim)
+      }
+
+      print(p)
+    }
+  }
+
+  dev.off()
+}
---
+++ R/cyt.ttest.R
@@ -0,0 +1,80 @@
+#' Two Sample T-test Comparisons
+#'
+#' @param x.df A matrix or data frame consisting of continuous and categorical variables.
+#' @param scale Transformation option for continuous variables. Options are NULL (default) and "log2".
+#' When scale = "log2". the function will use two sample t-test, when scale = NULL, the function will use Mann-Whitney U test.
+#'
+#' @return Prints the p-values of comparisons conducted.
+#' @export
+#'
+#' @examples
+#' data.df = cytdata.df[,-c(1,4)]
+#' data.df = filter(data.df, Group != "ND", Treatment != "Unstimulated")
+#' cyt.ttests(data.df, scale = "log2")
+#' cyt.ttests(data.df)
+#'
+cyt.ttests = function(x.df, scale = NULL) {
+  # Take input and store it as its own data frame
+  x1.df = x.df
+  # Convert any character variables to factors
+  cat_vars = sapply(x1.df, is.character)
+  if(any(cat_vars)){
+    x1.df[cat_vars] = lapply(x1.df[cat_vars], as.factor)
+  }
+  # Categorical Predictors
+  cat_preds = sapply(x1.df, is.factor)
+  # Create a list to store column names with numeric data
+  cont_vars <- sapply(x1.df, is.numeric)
+  # Empty list to store test results
+  test_results = list()
+  # Apply log2 transformation if scale is "log2"
+  if(!is.null(scale) && scale == "log2"){
+    x1.df[cont_vars] = lapply(x1.df[cont_vars], function(x) log2(x))
+  }
+  # Perform tests based on user input
+  for(cat_var in names(x1.df)[cat_preds]){
+    for(outcome in names(x1.df)[cont_vars]){
+      if(length(levels(x1.df[[cat_var]])) == 1){
+        stop("Must have two levels in the categorical variable")
+      }
+      else if(length(levels(x1.df[[cat_var]])) > 2){
+        stop("Must have two levels in the categorical variable")
+      }
+      else{
+
+        if(length(levels(x1.df[[cat_var]])) == 2){
+          # Two-sample t-test or Mann-Whitney U test
+          group1 <- x1.df[[outcome]][x1.df[[cat_var]] == levels(x1.df[[cat_var]])[1]]
+          group2 <- x1.df[[outcome]][x1.df[[cat_var]] == levels(x1.df[[cat_var]])[2]]
+
+          if(length(group1) < 2 || length(group2) < 2){
+            cat("Skipping test due to insufficient data in one of the groups\n")
+            next
+          }
+          # Check for low variance
+          else if(sd(group1) == 0 || sd(group2) == 0){
+            cat("Skipping test due to low variance in", outcome, "\n")
+            next
+          }
+          comparison_name <- paste(levels(x1.df[[cat_var]])[1], "vs", levels(x1.df[[cat_var]])[2])
+          formula <- as.formula(paste(outcome, "~", cat_var))
+
+          if(!is.null(scale) && scale == "log2"){
+            # Perform two-sample t-test
+            test_result <- t.test(formula, data = x1.df)
+            cat(paste0("T-test p-value for ", comparison_name, " on ", outcome, ": ", test_result$p.value, "\n"))
+          }
+          else if(is.null(scale)) {
+            # Perform Mann-Whitney U test
+            test_result <- wilcox.test(formula, data = x1.df)
+            cat(paste0("Mann-Whitney U test p-value for ", comparison_name, " on ", outcome, ": ", test_result$p.value, "\n"))
+          }
+
+          # Extract p-value and store in results list
+          result_key <- paste(outcome, cat_var, sep = "_")
+          test_results[[result_key]] <- test_result$p.value
+        }
+      }
+    }
+  }
+}
